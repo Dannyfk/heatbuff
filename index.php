71eac7067de8c1eec003e06aa22a148165932d24
Dannyfk <dfk2306@hotmail.com>
2017-12-14 15:11:26 +0100

    Merge and fix

--- index.php
+++ index.php
@@ -1,18 +1,46 @@
 <html lang="en">
-<<<<<<< HEAD
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Envavo</title>
-    <link rel="stylesheet" type="text/css" href="css/landingpageDanny.css">
-</head>
-<body>
-    <nav class="mainNav"></nav>
-    <header class="mainHeader"
-    ></header>
-    <section class="coldHand"></section>
-    <section class="solutionHere">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="css/Landing-Page-Chris.css">
+        <link rel="stylesheet" type="text/css" href="css/section-scroll.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:600" rel="stylesheet">
+        
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/jquery.section-scroll.js"></script>
+        <link rel="stylesheet" type="text/css" href="css/landingpageDanny.css">
+        <title>Envavo</title>
+    </head>
+    <body>
+        <nav class="mainNav">
+            <ul>
+                <div id="logo"></div>
+                <li>HeatBuff</li>
+                <li>Learn More</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <header class="mainHeader scrollable-section" data-section-title="HeatBuff"
+                >
+            <h1>Heatbuff</h1>
+            <div class="backgroundImage"></div>
+            <h3>No more cold hands</h3>
+            <div class='mouse-container'>
+                <div class='mouse'>
+                    <span class='scroll-down'></span>
+                </div>
+            </div>
+        </header>
+        <section class="coldHand scrollable-section"  data-section-title="Feeling cold?">
+            <h2> Ever been too cold? </h2>
+            <ul>
+                <li id="first-par">probably you wrapped yourself in the blankets,<br> put the heater up but still made typo after typo.</li>
+                <li id="second-par">It is because the problem is not the temperature... <br>But in your bloodflow.</li>
+            </ul>
+        </section>
+        <section class="solutionHere">
         <img class="solutionHereImage" src="image/hands_background.jpg" alt="Solution is here image">
         <aside id="solutionHereAside">
             <h2>The solution is here</h2>
@@ -46,54 +74,8 @@
         </article>
     </section>
     <script src="js\jquery-3.2.1.min.js"></script>
+    <script src="js/script-Chris.js"></script>
     <script src="js\landingpageScriptDanny.js"></script>
-
-</body>
-=======
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css/Landing-Page-Chris.css">
-        <link rel="stylesheet" type="text/css" href="css/section-scroll.css">
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:600" rel="stylesheet">
-        <script src="js/jquery-3.2.1.min.js"></script>
-        <script src="js/jquery.section-scroll.js"></script>
-        <title>Envavo</title>
-    </head>
-    <body>
-        <nav class="mainNav">
-            <ul>
-                <div id="logo"></div>
-                <li>HeatBuff</li>
-                <li>Learn More</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-        <header class="mainHeader scrollable-section" data-section-title="HeatBuff"
-                >
-            <h1>Heatbuff</h1>
-            <div class="backgroundImage"></div>
-            <h3>No more cold hands</h3>
-            <div class='mouse-container'>
-                <div class='mouse'>
-                    <span class='scroll-down'></span>
-                </div>
-            </div>
-        </header>
-        <section class="coldHand scrollable-section"  data-section-title="Feeling cold?">
-            <h2> Ever been too cold? </h2>
-            <ul>
-                <li id="first-par">probably you wrapped yourself in the blankets,<br> put the heater up but still made typo after typo.</li>
-                <li id="second-par">It is because the problem is not the temperature... <br>But in your bloodflow.</li>
-            </ul>
-        </section>
-        <section class="solutionHere" data-section-title="Solution"></section>
-        <section class="heatBuff"  data-section-title="Warm Hands"></section>
-        <section class="warmHands" data-section-title="Speed"></section>
-        <section class="customAngles" data-section-title="Customizable angles"></section>
-        <script src="js/script-Chris.js"></script>
+        
     </body>
->>>>>>> af6768a312f5221b1bf5330d6168a4165317f802
 </html>
