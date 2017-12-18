e9cae302cf53c7b7707fcd2100e7eeff97f28709
KristupasMat <kristupom@gmail.com>
2017-12-18 21:23:06 +0100

    navigation changes

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,9 +1,9 @@
 <nav class="mainNav">
-            <ul>
-                <div id="logo"><img src="image/logo.png" alt=""></div>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="productPage.php">Heatbuff</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
+    <ul>
+        <li class="companyName"><a href="index.php">envavo</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="productPage.php">Heatbuff</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
